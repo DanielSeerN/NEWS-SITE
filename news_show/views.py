f2f1256fbf3e698ba1670733da4517cba7a83318
root <danya.sitovich@mail.ru>
2021-09-26 12:53:00 +0300

    Сделан прототип главной странички, немного переделаны модели

--- news_show/views.py
+++ news_show/views.py
@@ -1,3 +1,29 @@
+import json
+
 from django.shortcuts import render
+from django.views import View
+from .models import NewsArticle, LatestNews, Category
+
+
+
+def get_news(request, *args, **kwargs):
+        with open('news_files/cyberspostru_news.json', 'r', encoding='utf-8') as file:
+            cybersport_ru_news = json.load(file)
+            all_news = []
+            for news_article in cybersport_ru_news:
+                news_object = NewsArticle()
+                news_object.title = news_article['article_title']
+                news_object.category = news_article['article_category']
+                news_object.news_link = news_article['article_url']
+                news_object.post_time = news_article['article_post_time']
+                all_news.append(news_object)
+            context = {
+                'all_news': all_news
+            }
+            return render(request, 'news_show/index.html', context)
+
+
+def view(request):
+    return render(request, 'news_show/index.html')
 
 # Create your views here.
