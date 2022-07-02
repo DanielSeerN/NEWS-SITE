a8b24d9ab24e88a0d04473c487e0b26a595de24c
DanielSeerN <danya.sitovich@mail.ru>
2022-07-03 02:10:10 +0300

    рефакторинг, поправочки

--- news_show/parsers/cybersports_ru_parser.py
+++ news_show/parsers/cybersports_ru_parser.py
@@ -14,46 +14,50 @@ def get_html(url):
     return response
 
 
-def get_news(html):
+def make_soup(html):
+    """
+    Создание объекта BeatifulSoup
+    """
+    soup = BeautifulSoup(html, 'lxml')
+    return soup
+
+
+def get_news():
     """
     Обработка HTML и сохранение нужных данных.
     """
     data_dict = []
+    html = get_html(URL)
     soup = BeautifulSoup(html, 'lxml')
-    news = soup.find_all('div', class_="cs-col-lg-6 cs-col-md-6 cs-col-xs-12 margin-bottom--20")
-    for up_article in news:
-        article_url = up_article.find('a',
-                                      class_='inverse-color--black-00').get(
-            'href')
-        if article_url.split('/')[2] == 'match':
-            continue
-        if article_url.split('/')[2] != 'www.cybersport.ru':
-            article_url = URL + article_url
-        article_title = up_article.find('h3',
-                                        class_='margin-bottom--0 margin-top--10 fz--22 card-vertical__title').find(
-            'a').text
-
-        article_category = up_article.find('a', class_='tag').text
-        response = requests.get(article_url).text
-        soup = BeautifulSoup(response, 'lxml')
-        article_paragraphs = soup.find('section', class_='article__inner').find_all('p')
-        article_post_time = soup.find('time', itemprop='datePublished').text
-        article_text = []
-        for paragraph in article_paragraphs:
-            paragraph_text = paragraph.text
-            article_text.append(paragraph_text)
-        article_text = '*'.join(article_text)  # Соединяем текст через звезду, чтобы потом легко было его разделить.
+    articles = soup.find_all('a', class_='link_CocWY')
+    urls = []
+    for url in articles:
+        article_url = url.get('href')
+        urls.append(URL + article_url)
+    for url in urls:
+        article_html = get_html(url)
+        article_soup = make_soup(article_html)
+        article_title = article_soup.find(class_='headerContentWrap_2xAno').find('h1').text
+        article_post_time = article_soup.find(class_='materialPub_wFwM0').text
+        article_category = article_soup.find(class_='root_0S2Nc tag_dcScC').text
+        article_text_tags = article_soup.find(
+            class_='text-content js-mediator-article js-mediator-article root_sK2zH content_5HuK5').contents
+        text = ''
+        for paragraph in article_text_tags:
+            text += paragraph.text
+            text += '*'
         hot_news = {
             'article_title': article_title,
-            'article_url': article_url,
+            'article_url': url,
             'article_post_time': article_post_time,
             'article_category': article_category,
             'article_source': 'Cybersport.ru',
-            'article_mark': article_url.split('/')[-1],
-            'article_text': article_text,
+            'article_mark': url.split('/')[-1],
+            'article_text': text,
         }
         data_dict.append(hot_news)
-    write_json(data_dict)
+
+    return data_dict
 
 
 def write_json(news):
@@ -68,7 +72,8 @@ def parse_cybersport_ru_news():
     """
     Функция вызова парсера.
     """
-    get_news(get_html(URL))
+    data = get_news()
+    write_json(data)
 
 
 if __name__ == '__main__':
--- news_show/views.py
+++ news_show/views.py
@@ -6,99 +6,110 @@ from django.views import View
 from .models import FavouriteArticle, Reader, Comment
 from .forms import LoginForm, RegistrationForm, CommentForm
 from .services import get_reader, get_article_by_slug, get_category, get_articles_by_category, get_all_categories, \
-    get_all_news
+    get_all_news, get_favorite_articles
 
 
-def do_main_news(request):
+class MainPage(View):
     """
     Главная страница агрегатора.
     """
-    all_news = get_all_news()
-    context = {
-        'all_news': all_news
-    }
-    return render(request, 'news_show/index.html', context)
+
+    def get(self, request):
+        all_news = get_all_news()
+        context = {
+            'all_news': all_news
+        }
+        return render(request, 'news_show/index.html', context)
 
 
-def get_categories(request):
+class CategoryPage(View):
     """
     Представление для категорий новостей.
     """
-    categories = get_all_categories()
-    context = {
-        'categories': categories
-    }
-    return render(request, 'news_show/categories.html', context)
+
+    def get(self, request):
+        categories = get_all_categories()
+        context = {
+            'categories': categories
+        }
+        return render(request, 'news_show/categories.html', context)
 
 
-def get_category_news(request, **kwargs):
+class CategoryNewsPage(View):
     """
     Представление для новостей категории.
     """
-    category = get_category(kwargs)
-    news = get_articles_by_category(category)
-    context = {
-        'news': news,
-        'category': category
-    }
-    return render(request, 'news_show/category_news.html', context)
+
+    def get(self, request, **kwargs):
+        category = get_category(kwargs)
+        news = get_articles_by_category(category)
+        context = {
+            'news': news,
+            'category': category
+        }
+        return render(request, 'news_show/category_news.html', context)
 
 
-def add_favourite_article(request, **kwargs):
-    """
-    Представление для добавления новости в избранное
-    """
-    article = get_article_by_slug(kwargs)
-    reader = get_reader(request)
-    favourite_article, created = FavouriteArticle.objects.get_or_create(article=article, reader=reader)
-    if created:
-        pass
-    else:
-        reader.favourite_articles.add(favourite_article)
-        favourite_article.save()
-    return HttpResponseRedirect('/')
+class AddFavouriteArticle(View):
+    def post(self, request, **kwargs):
+        """
+        Представление для добавления новости в избранное
+        """
+        article = get_article_by_slug(kwargs)
+        reader = get_reader(request)
+        favourite_article, created = FavouriteArticle.objects.get_or_create(article=article, reader=reader)
+        if created:
+            pass
+        else:
+            reader.favourite_articles.add(favourite_article)
+            favourite_article.save()
+        return HttpResponseRedirect('/')
 
 
-def get_favourite_articles(request):
+class FavouriteArticlesPage(View):
     """
     Представление для отображения избранных новостей пользователя.
     """
-    reader = get_reader(request)
-    reader_favourites = FavouriteArticle.objects.filter(reader=reader)
-    context = {
-        'favourites': reader_favourites
-    }
-    return render(request, 'news_show/reader_fav_articles.html', context)
 
-
-def show_article(request, **kwargs):
-    """
-    Представление для отображения статьи.
-    """
-    article = get_article_by_slug(kwargs)
-    article_text = article.text.split('*')
-    form = CommentForm(request.POST or None)
-    comments = Comment.objects.filter(article=article)
-    context = {
-        'article': article,
-        'article_text': article_text,
-        'form': form,
-        'comments': comments
-    }
-    return render(request, 'news_show/article.html', context)
-
-
-def leave_a_comment(request, **kwargs):
-    """
-    Представление для создания комментария
-    """
-    article = get_article_by_slug(kwargs)
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        comment_text = form.cleaned_data['text']
+    def get(self, request):
         reader = get_reader(request)
-        Comment.objects.create(text=comment_text, reader=reader, article=article)
-    return HttpResponseRedirect(f'/articles/{article.slug}')
+        reader_favourites = get_favorite_articles(reader)
+        context = {
+            'favourites': reader_favourites
+        }
+        return render(request, 'news_show/reader_fav_articles.html', context)
+
+
+class ArticlePage(View):
+    def get(self, request, **kwargs):
+        """
+        Представление для отображения статьи.
+        """
+        article = get_article_by_slug(kwargs)
+        article_text = article.text.split('*')
+        form = CommentForm(request.POST or None)
+        comments = Comment.objects.filter(article=article)
+        context = {
+            'article': article,
+            'article_text': article_text,
+            'form': form,
+            'comments': comments
+        }
+        return render(request, 'news_show/article.html', context)
+
+
+class LeaveComment(View):
+    def get(self, request, **kwargs):
+        """
+        Представление для создания комментария
+        """
+        article = get_article_by_slug(kwargs)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment_text = form.cleaned_data['text']
+            reader = get_reader(request)
+            Comment.objects.create(text=comment_text, reader=reader, article=article)
+        return HttpResponseRedirect(f'/articles/{article.slug}')
 
 
 class LoginView(View):
@@ -106,7 +117,7 @@ class LoginView(View):
     Представление для входа в учётную запись.
     """
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         form = LoginForm(request.POST or None)
 
         context = {
@@ -115,7 +126,7 @@ class LoginView(View):
 
         return render(request, 'news_show/login.html', context)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = LoginForm(request.POST or None)
         if form.is_valid():
             username = form.cleaned_data['username']
@@ -136,21 +147,21 @@ class RegistrationView(View):
     Представление для регистрации учётной записи.
     """
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         form = RegistrationForm(request.POST or None)
         context = {
             'form': form
         }
         return render(request, 'news_show/registration.html', context)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = RegistrationForm(request.POST or None)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = form.cleaned_data['username']
             user.set_password(form.cleaned_data['password'])
             user.save()
-            reader = Reader.objects.create(user=user, mail_box=user.email)
+            Reader.objects.create(user=user, mail_box=user.email)
             user = authenticate(username=user.username, password=form.cleaned_data['password'])
             login(request, user)
             return redirect('/')
